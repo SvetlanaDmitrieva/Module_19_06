20dfbfd975e208ab5c23d650623d67630442b39c
SvetlanaDmitrieva <sv-dmitrieva@list.ru>
2024-12-15 19:00:41 +0300

    15/12-02

---
+++ UrbanDjango/app/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Buyer(models.Model):
+    objects = None
+    name = models.CharField(max_length=100)  # Имя покупателя
+    balance = models.DecimalField(max_digits=10, decimal_places=2)  # Баланс
+    age = models.IntegerField()  # Возраст
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    objects = None
+    title = models.CharField(max_length=100)  # Название игры
+    cost = models.DecimalField(max_digits=10, decimal_places=2)  # Цена игры
+    size = models.DecimalField(max_digits=10, decimal_places=2)  # Размер файлов игры
+    description = models.TextField()  # Описание игры
+    age_limited = models.BooleanField(default=False)  # Ограничение по возрасту
+    buyers = models.ManyToManyField(Buyer, related_name='games')  # Покупатели
+
+    def __str__(self):
+        return self.title
